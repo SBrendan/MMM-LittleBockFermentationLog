8e4008d8e03d58544c7f3c45be5cf55f521c5e13
Brendan Seiller <seillerbrendan@gmail.com>
2024-10-16 22:04:36 +0200

    (feat) improve cache management and first load

--- node_helper.js
+++ node_helper.js
@@ -7,114 +7,124 @@ module.exports = NodeHelper.create({
         console.log("Node helper started for MMM-LittleBockFermentationLog");
     },
 
-    socketNotificationReceived: function(notification, payload) {
+    socketNotificationReceived: async function(notification, payload) {
         console.log("Notification received:", notification);
-        if (notification === "MMM-LittleBockFermentationLog_GET_FERMENTATION_DATA") {
-            this.config = payload;
-            const apiUrl = `${this.config.apiUrl}/fermentation_logs?brewSession=${this.config.brewSessionID}`;
-            this.getDataFromCacheOrFetch(apiUrl)
-                .then(data => {
-                    this.sendSocketNotification("MMM-LittleBockFermentationLog_FERMENTATION_DATA_RESULT", data);
-                })
-                .catch(error => {
-                    console.error("Error fetching fermentation data:", error);
-                    this.sendSocketNotification("MMM-LittleBockFermentationLog_FERMENTATION_DATA_RESULT", []);
-                });
-        }
-
-        if (notification === "MMM-LittleBockFermentationLog_GET_RECIPE_DATA") {
-            this.config = payload;
-            const brewSessionUrl = `${this.config.apiUrl}/brew_sessions/${this.config.brewSessionID}`;
-            const recipeData = this.getRecipeDataFromCacheOrFetch(brewSessionUrl);
-            this.sendSocketNotification("MMM-LittleBockFermentationLog_RECIPE_DATA_RESULT", recipeData || {});
+        try {
+            if (notification === "MMM-LittleBockFermentationLog_GET_FERMENTATION_DATA") {
+                this.config = payload;
+                const apiUrl = `${this.config.apiUrl}/fermentation_logs?brewSession=${this.config.brewSessionID}`;
+                const data = await this.getDataFromCacheOrFetch(apiUrl);
+                this.sendSocketNotification("MMM-LittleBockFermentationLog_FERMENTATION_DATA_RESULT", data);
+            }
+    
+            if (notification === "MMM-LittleBockFermentationLog_GET_RECIPE_DATA") {
+                this.config = payload;
+                const brewSessionUrl = `${this.config.apiUrl}/brew_sessions/${this.config.brewSessionID}`;
+                const recipeData = await this.getRecipeDataFromCacheOrFetch(brewSessionUrl);
+                this.sendSocketNotification("MMM-LittleBockFermentationLog_RECIPE_DATA_RESULT", recipeData);
+            }
+        } catch (error) {
+            console.error("Error processing notification:", error);
+            if (notification === "MMM-LittleBockFermentationLog_GET_FERMENTATION_DATA") {
+                this.sendSocketNotification("MMM-LittleBockFermentationLog_FERMENTATION_DATA_RESULT", []);
+            } else if (notification === "MMM-LittleBockFermentationLog_GET_RECIPE_DATA") {
+                this.sendSocketNotification("MMM-LittleBockFermentationLog_RECIPE_DATA_RESULT", {});
+            }
         }
     },
-
+    
     async fetchAllData(url) {
         const allData = [];
         let page = 1;
-
+    
         const fetchPage = async (page) => {
-            try {
-                console.log(`Fetching page ${page}...`);
-                const response = await fetch(`${url}&page=${page}`, {
-                    headers: {
-                        "X-AUTH-TOKEN": this.config.apiToken
-                    }
-                });
-
-                if (!response.ok) {
-                    throw new Error(`Failed to fetch page ${page}: ${response.statusText}`);
+            console.log(`Fetching page ${page}...`);
+            const response = await fetch(`${url}&page=${page}`, {
+                headers: {
+                    "X-AUTH-TOKEN": this.config.apiToken
                 }
-
-                const data = await response.json();
-                if (data && data['hydra:member']) {
-                    allData.push(...data['hydra:member']);
-
-                    if (data['hydra:view'] && data['hydra:view']['hydra:next']) {
-                        return fetchPage(page + 1);
-                    } else {
-                        cache.set(url, allData);
-                        cache.set('lastPage', page);
-                        console.log("All data fetched and cached.");
-                        return allData;
-                    }
+            });
+    
+            const data = await response.json();
+            if (data && data['hydra:member']) {
+                allData.push(...data['hydra:member']);
+                if (data['hydra:view'] && data['hydra:view']['hydra:next']) {
+                    return fetchPage(page + 1);
                 } else {
-                    console.warn(`No data on page ${page}`);
+                    cache.set(url, allData);
+                    cache.set('lastPage', page);
+                    console.log("All data fetched and cached.");
                     return allData;
                 }
-            } catch (error) {
-                console.error(`Error fetching all data at page ${page}:`, error);
+            } else {
+                console.warn(`No data on page ${page}`);
                 return allData;
             }
         };
-
         return fetchPage(page);
     },
+    
+    async checkForNewPages(url, cachedData) {
+        const lastPage = cache.get('lastPage') || 1;
+        const nextPage = lastPage + 1;
+        const response = await fetch(`${url}&page=${nextPage}`, {
+            headers: {
+                "X-AUTH-TOKEN": this.config.apiToken
+            }
+        });
+        const data = await response.json();
+        if (data['hydra:member'] && data['hydra:member'].length > 0) {
+            cachedData.push(...data['hydra:member']);
+            cache.set(url, cachedData);
+            cache.set('lastPage', nextPage);
+            console.log(`Page ${nextPage} added to cache.`);
+        } else {
+            console.log("No new pages found.");
+        }
+        return cachedData;
+    },
 
-    getDataFromCacheOrFetch(url) {
+    async checkForUpdatesOnLastPage(url, cachedData) {
+        const lastPage = cache.get('lastPage') || 1;
+        const response = await fetch(`${url}&page=${lastPage}`, {
+            headers: {
+                "X-AUTH-TOKEN": this.config.apiToken
+            }
+        });
+    
+        if (!response.ok) {
+            console.error(`Failed to fetch last page (${lastPage}): ${response.statusText}`);
+            return cachedData;
+        }
+    
+        const data = await response.json();
+        if (data['hydra:member'] && data['hydra:member'].length > cachedData.length) {
+            console.log("New data found on the last page, updating cache...");
+            
+            const newEntries = data['hydra:member'].slice(cachedData.length); 
+            cachedData.push(...newEntries);
+            
+            cache.set(url, cachedData);
+            console.log("Cache updated with new entries from the last page.");
+        } else {
+            console.log("No new data found on the last page.");
+        }
+    
+        return cachedData;
+    },   
+
+    async getDataFromCacheOrFetch(url) {
         const cachedData = cache.get(url);
         if (cachedData) {
-            console.log("Using cached data for fermentation logs.");
-            return this.checkForNewPages(url, cachedData);
+            console.log("Checking last page for new data...");
+            const updatedData = await this.checkForUpdatesOnLastPage(url, cachedData);
+            return this.checkForNewPages(url, updatedData); 
         } else {
             console.log("Cache expired or missing, fetching all data...");
             return this.fetchAllData(url);
         }
     },
-
-    async checkForNewPages(url, cachedData) {
-        const lastPage = cache.get('lastPage') || 1;
-        const nextPage = lastPage + 1;
-
-        try {
-            const response = await fetch(`${url}&page=${nextPage}`, {
-                headers: {
-                    "X-AUTH-TOKEN": this.config.apiToken
-                }
-            });
-
-            if (!response.ok) {
-                throw new Error(`Failed to fetch new page ${nextPage}: ${response.statusText}`);
-            }
-
-            const data = await response.json();
-            if (data['hydra:member'] && data['hydra:member'].length > 0) {
-                console.log(`New page ${nextPage} detected, updating cache...`);
-                cachedData.push(...data['hydra:member']);
-                cache.set(url, cachedData);
-                cache.set('lastPage', nextPage);
-            } else {
-                console.log("No new pages found.");
-            }
-
-            return cachedData;
-        } catch (error) {
-            console.error("Error checking for new pages:", error);
-            return cachedData;
-        }
-    },
-
+    
     getRecipeDataFromCacheOrFetch(brewSessionUrl) {
         const cachedRecipeData = cache.get(brewSessionUrl);
         if (cachedRecipeData) {
